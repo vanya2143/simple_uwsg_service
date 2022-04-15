d82dbfe7050b0e24eddac3bf7e48ecb5ff17a81b
Ivan Bielozorov <vanya2143@gmail.com>
2022-04-15 18:53:46 +0200

    refactoring

--- app/app.py
+++ app/app.py
@@ -1,31 +1,19 @@
 import json
-import re
-
-import app.utils as utils
 from urllib.parse import parse_qs
 
-
-def index(environ, start_response):
-    status = '200 OK'
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-    ]
-    start_response(status, response_headers)
-    return ['Hello'.encode('utf-8')]
+import app.utils as utils
 
 
 def sum_of_two(environ, start_response):
-    parameters = parse_qs(environ.get('QUERY_STRING', ''))
+    params = parse_qs(environ.get('QUERY_STRING', ''))
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'application/json'),
     ]
 
-    arr = list(map(int, parameters.get('arr', [])))
-    result = utils.sum_of_two(arr)
-
+    arr = list(map(int, params.get('arr', [])))
     response = {
-        'sum_of_two': result
+        'sum_of_two': utils.sum_of_two(arr)
     }
 
     start_response(status, response_headers)
@@ -33,37 +21,43 @@ def sum_of_two(environ, start_response):
 
 
 def paired_with_dict(environ, start_response):
+    if environ['REQUEST_METHOD'] == 'POST':
+        try:
+            request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        except ValueError:
+            request_body_size = 0
+
+        request_body = environ['wsgi.input'].read(request_body_size)
+
+        response = 'Post data'
+    else:
+        response = 'Get data'
+
     status = '200 OK'
     response_headers = [
-        ('Content-Type', 'text/plain'),
+        ('Content-Type', 'text/json'),
     ]
     start_response(status, response_headers)
-    return [environ.get('QUERY_STRING', '').encode('utf-8'), environ.get('RAW_URI', '').encode('utf-8')]
+    return [json.dumps(response).encode('utf-8')]
 
 
-urls = [
-    # (r'^$', index),
-    (r'sum_of_two/?$', sum_of_two),
-    (r'paired_with_dict/?$', paired_with_dict)
-]
+routes = {
+    '/sum_of_two': sum_of_two,
+    '/paired_with_dict': paired_with_dict,
+}
 
 
-def not_found(environ, start_response):
-    status = '404 NOT FOUND '
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-    ]
-    start_response(status, response_headers)
-    return ['not found'.encode('utf-8')]
+class Application:
+    def __init__(self, routes):
+        self.routes = routes
+
+    def not_found(self, environ, start_response):
+        start_response('404 Not Found', [('Content-Type', 'text/plain')])
+        return ['404 Not Found']
+
+    def __call__(self, environ, start_response):
+        handler = self.routes.get(environ.get('PATH_INFO', '').rstrip('/')) or self.not_found
+        return handler(environ, start_response)
 
 
-def application(environ, start_response):
-    path = environ.get('PATH_INFO', '').lstrip('/')
-    for regex, callback in urls:
-        match = re.search(regex, path)
-        if match is not None:
-            return callback(environ, start_response)
-    return not_found(environ, start_response)
-
-
-# uwsgi --socket 127.0.0.1:8080 --protocol=http --wsgi-file app/app.py
+app = Application(routes)
