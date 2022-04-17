2e430c0c971a2e8b6f3293cdb7be4e7015de64ea
Ivan Bielozorov <vanya2143@gmail.com>
2022-04-17 17:02:26 +0200

    added comments in code

--- app/app.py
+++ app/app.py
@@ -5,12 +5,21 @@ import app.utils as utils
 
 
 def sum_of_two(environ, start_response):
+    """
+    Parse query string for arr params like ?arr=1&arr=2&arr=3
+    :returns json:
+    {
+        "sum_of_two": true
+    }
+    """
+    # get and parse query string
     params = parse_qs(environ.get('QUERY_STRING', ''))
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'application/json'),
     ]
 
+    # Change type of elements str to int in array
     arr = list(map(int, params.get('arr', [])))
     response = {
         'sum_of_two': utils.sum_of_two(arr)
@@ -51,10 +60,15 @@ class Application:
         self.routes = routes
 
     def not_found(self, environ, start_response):
+        """
+        If client pass unaccepted url path
+        :return:
+        """
         start_response('404 Not Found', [('Content-Type', 'text/plain')])
         return ['404 Not Found']
 
     def __call__(self, environ, start_response):
+        # Matching url path in routes and returns func-handler obj or not_found
         handler = self.routes.get(environ.get('PATH_INFO')) or self.not_found
         return handler(environ, start_response)
 
